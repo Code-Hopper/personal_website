42a50d733a637081df181c0e6c73eadc06886cbc
code_hopper <ameykhondekar01@gmail.com>
2024-05-29 19:20:28 +0530

    about number counter animation using js

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,65 @@ const rotateTextFunction = () => {
 rotateTextFunction();
 
 // create and append new span as new child in original HTML document
+
+// counting number animation about numbers grid
+
+let aboutNumbersGridChilds = $(".about-grid-child .about-number")
+
+
+// console.log(aboutNumbersGridChilds)
+
+
+
+
+let start = 0
+
+let maxEnd = 0
+
+let time = 1000
+
+
+for (let child of aboutNumbersGridChilds) {
+
+    let end = Number(child.dataset.number)
+
+    if(maxEnd < end){
+        maxEnd = end 
+    }
+
+}
+
+let adjustedTime = Math.round(maxEnd/time)
+
+// relative time
+// in relation to the max value
+
+
+for(let child of aboutNumbersGridChilds){
+    startCounter(child,Number(child.dataset.number))
+}
+
+function startCounter(target,end) {
+
+    let count = 0
+
+    console.log(end)
+
+    let countInterval = setInterval(() => {
+
+        if (count <= end) {
+
+            target.innerHTML = `${count}+`
+
+            // console.log(target)
+
+            count++
+
+        } else {
+            clearInterval(countInterval)
+        }
+
+    }, adjustedTime)
+}
+
+// startCounter()
